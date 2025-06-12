220d9b7ce971cb57c5924126e0172a0a1da2cd22
Robert Carr <robert.carr@slalom.com>
2025-06-12 14:36:05 -0700

    Remove imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { About } from "./components/About";
-import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-import { Navbar } from "./components/Navbar";
 import { ScrollToTop } from "./components/ScrollToTop";
-import { Services } from "./components/Services";
 import "./App.css";
 
 function App() {
